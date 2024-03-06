576cdd5efcaa17d589e02c399e3155360154c89c
olenkapyrih <olena.pyrih.kn.2021@lpnu.ua>
2024-03-06 17:52:52 +0200

    type annotation added

--- format_detector.py
+++ format_detector.py
@@ -1,7 +1,7 @@
 import magic
 
 
-def format_detector(file_path):
+def format_detector(file_path: str) -> str:
     mime = magic.Magic()
     file_type = mime.from_file(file_path)
     return file_type
